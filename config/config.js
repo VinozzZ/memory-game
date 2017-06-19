b79fdd1ea078c527b541f7478b4dfc264980903b
YINGRONG ZHAO <lionzhao0820@gmail.com>
2017-06-18 21:48:14 -0400

    Rebuilt the game with express and mysql

---
+++ config/config.js
@@ -0,0 +1,13 @@
+var serverInfo = {
+	host: "127.0.0.1",
+	username: 'x',
+	password: 'x',
+	database:'memory_game',
+	session:{
+		secret: 'sofia730',
+  		resave: false,
+  		saveUninitialized: true
+	}
+}
+
+module.exports = serverInfo;
